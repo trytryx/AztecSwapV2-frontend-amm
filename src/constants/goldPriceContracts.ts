1a283b5acf4b15cfeb1bcdd2975db1f4c401fcf7
trytryx <100495047+trytryx@users.noreply.github.com>
2023-05-02 02:04:57 +0300

    Update goldPriceContracts.ts

--- src/constants/goldPriceContracts.ts
+++ src/constants/goldPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0x8298fea38aa8627316ffd943fe001b69cac8174d',
-  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0xb50539EAC8971052a96A2E466C6865b942835C56'
+  cakeAddress: '0xcBB53a1b4c1728972De4308B279e18EF941EdA2D',
+  busdAddress: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+  lpAddress: '0xB1C726820fAB8478d4d8a06abFc710f3732e6587'
 }
 
 export default priceContracts
